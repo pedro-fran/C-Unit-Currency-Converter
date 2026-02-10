13f97c36afdf0a1e17bd3f5857106cf6d44fffba
pedro-fran <pedro.franca169@gmail.com>
2026-02-10 18:40:23 +0000

    Update main menu and unit converter

---
+++ main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "converters.h"
+
+void handleMeasureUnits();
+
+int main() {
+    int   option;
+
+    while (1) {
+        printf("\n===========================\n");
+        printf("    CONVERTER MAIN MENU      \n");
+        printf("===========================\n");
+        printf("1.  Measure Units\n");
+        printf("2.  Currencies\n");
+        printf("3.  Exit\n");
+        
+        printf("Select an option : ");
+        if (scanf("%d", &option) != 1) {
+            printf("\nInvalid input! Please enter a number (1-3) corresponding to one of the options.\n");
+            while (getchar() != '\n');
+            continue;
+        }
+
+        switch (option) {
+            case 1 : handleMeasureUnits(); break;
+            //case 2 : handleCurrency(); break;
+            case 3 : printf("\nGoodbye!\n"); return 0;
+            default: printf("\nOption not found! Please enter a number (1-3) corresponding to one of the options.\n");
+        }
+    }
+}
+
+void handleMeasureUnits() {
+    int   subOption;
+    // metric to imperial e vice versa
+    while (1) {
+        printf("\n>>> Measure Units <<<\n");
+        printf("1. Length\n");
+        printf("2. Mass\n");
+        printf("3. Temperature\n");
+        printf("4. Time\n");
+        printf("5. Volume\n");
+        printf("0. Back to Main Menu\n");
+
+        printf("Select an option : ");
+            if (scanf("%d", &subOption) != 1) {
+                printf("\nInvalid input! Please enter a number (1-5) corresponding to one of the options.\n");
+                while (getchar() != '\n');
+                continue;
+            }
+            switch (subOption) {
+                case 0 : return; break; /*
+                case 1 : handleLength(); break;
+                case 2 : handleMass(); break;
+                case 3 : handleTemperature(); break;
+                case 4 : handleTime(); break;
+                case 5 : handleVolume(); break;*/
+                default: printf("\nOption not found! Please enter a number (1-5) corresponding to one of the options.\n");
+            }
+    }
+}
